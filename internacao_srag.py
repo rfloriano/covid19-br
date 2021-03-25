61f0a0700a994ed53dc69e7157640ae8ea47ff04
rfloriano <rflorianobr@gmail.com>
2021-03-25 01:10:33 -0300

    22k it/s

--- internacao_srag.py
+++ internacao_srag.py
@@ -1,3 +1,5 @@
+import io
+import os
 import argparse
 import csv
 from pathlib import Path
@@ -40,10 +42,13 @@ def get_csv_resources(dataset_name):
             yield resource["url"]
 
 
-def download_files():
+def download_files(should_skip=False):
     for dataset in SRAG_DATASETS:
         csv_url = next(get_csv_resources(dataset))
         filename = DOWNLOAD_PATH / (Path(csv_url).name + ".gz")
+        if should_skip and os.path.isfile(filename):
+            yield filename
+            return
         rows.utils.download_file(
             csv_url,
             filename=filename,
@@ -54,36 +59,38 @@ def download_files():
 
 
 def convert_row(row):
-    new = {}
-    for key, value in row.items():
-        key, value = key.lower(), value.strip()
-        if len(value[value.rfind("/") + 1 :]) == 3:
-            # TODO: e se for 2021?
-            value = value[: value.rfind("/")] + "/2020"
-        if not value:
-            value = None
-        elif key.startswith("dt_"):
-            value = PtBrDateField.deserialize(value)
-        new[key] = value
+    # new = {}
+    # for key, value in row.items():
+    #     key, value = key.lower(), value.strip()
+    #     if len(value[value.rfind("/") + 1:]) == 3:
+    #         # TODO: e se for 2021?
+    #         value = value[: value.rfind("/")] + "/2020"
+    #     if not value:
+    #         value = None
+    #     elif key.startswith("dt_"):
+    #         value = PtBrDateField.deserialize(value)
+    #     new[key] = value
 
     # TODO: refatorar código que calcula diferença em dias
 
     diff_days = None
-    if new["evolucao"] == "2" and None not in (new["dt_evoluca"], new["dt_interna"]):
-        diff_days = (new["dt_evoluca"] - new["dt_interna"]).days
-    new["dias_internacao_a_obito_srag"] = diff_days
+    if row["DT_EVOLUCA"] and row["DT_INTERNA"]:
+        dt_evo = PtBrDateField.deserialize(row["DT_EVOLUCA"])
+        dt_int = PtBrDateField.deserialize(row["DT_INTERNA"])
+        diff_days = (dt_evo - dt_int).days
 
-    diff_days = None
-    if new["evolucao"] == "3" and None not in (new["dt_evoluca"], new["dt_interna"]):
-        diff_days = (new["dt_evoluca"] - new["dt_interna"]).days
-    new["dias_internacao_a_obito_outras"] = diff_days
+    row["DIAS_INTERNACAO_A_OBITO_SRAG"] = None
+    row["DIAS_INTERNACAO_A_OBITO_OUTRAS"] = None
+    row["DIAS_INTERNACAO_A_ALTA"] = None
 
-    diff_days = None
-    if new["evolucao"] == "1" and None not in (new["dt_evoluca"], new["dt_interna"]):
-        diff_days = (new["dt_evoluca"] - new["dt_interna"]).days
-    new["dias_internacao_a_alta"] = diff_days
+    if row["EVOLUCAO"] == "2":
+        row["DIAS_INTERNACAO_A_OBITO_SRAG"] = diff_days
+    elif row["EVOLUCAO"] == "3":
+        row["DIAS_INTERNACAO_A_OBITO_OUTRAS"] = diff_days
+    elif row["EVOLUCAO"] == "1":
+        row["DIAS_INTERNACAO_A_ALTA"] = diff_days
 
-    new["faixa_etaria"] = calculate_age_range(new["nu_idade_n"])
+    row["FAIXA_ETARIA"] = calculate_age_range(row["NU_IDADE_N"])
 
     # TODO: adicionar coluna ano e semana epidemiológica
     # TODO: corrigir RuntimeError: ERROR:  invalid input syntax for integer: "20-1"
@@ -91,18 +98,44 @@ def convert_row(row):
     # TODO: data nascimento (censurar?)
     # TODO: dt_interna: corrigir valores de anos inexistentes
 
-    return new
+    return row
+
+
+class CsvLazyDictWriter(rows.utils.CsvLazyDictWriter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._fobj = io.StringIO()
+
+    def writerows(self, rows):
+        self.writerows = self.writer.writerows
+        return self.writerows(rows)
 
 
 def main():
-    filenames = download_files()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--skip", help="Skip downloaded files",  action="store_true")
+    parser.add_argument("-p", "--processes", help="How many processes should be used to process file", type=int)
+    parser.add_argument("-l", "--lines", help="How many lines should be writed at time", type=int, default=1000)
+    args = parser.parse_args()
+
+    filenames = download_files(args.skip)
     output_filename = OUTPUT_PATH / "internacao_srag.csv.gz"
-    writer = rows.utils.CsvLazyDictWriter(output_filename)
+    writer = CsvLazyDictWriter(output_filename)
     for filename in filenames:
         with rows.utils.open_compressed(filename, encoding="utf-8") as fobj:
             reader = csv.DictReader(fobj, delimiter=";")
+            first = True
+            lines = []
             for row in tqdm(reader, desc=f"Converting {filename.name}"):
-                writer.writerow(convert_row(row))
+                if first:
+                    writer.writerow(convert_row(row))
+                    first = False
+                    continue
+                lines.append(convert_row(row))
+                if len(lines) > args.lines:
+                    writer.writerows(lines)
+                    lines = []
 
 
 if __name__ == "__main__":
